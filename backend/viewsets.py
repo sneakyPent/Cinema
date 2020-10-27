0495954a27d084c051fb945ba54a1ce8b0dbaa7f
sneakyPent <zaharioudakis@yahoo.com>
2020-10-28 01:53:24 +0200

    add create user in userviewset for registration

--- backend/viewsets.py
+++ backend/viewsets.py
@@ -8,12 +8,9 @@ from rest_framework.response import Response
 from rest_framework.status import *
 from django.db.models import Q, QuerySet, Prefetch
 
-from backend.permissions import CustomDjangoModelPermissions
+from backend.permissions import CustomDjangoModelPermissions, NotAuthenticatedCreateOnly
 from backend.serializers import *
 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
-
-# from backend.models import Request
 from django.contrib.auth.models import User
 
 
@@ -33,3 +30,59 @@ class CinemaViewSet(viewsets.ModelViewSet):
 	queryset = Cinema.objects.all()
 	serializer_class = CinemaSerializer
 	permission_classes = (CustomDjangoModelPermissions,)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
+	permission_classes = (CustomDjangoModelPermissions | NotAuthenticatedCreateOnly,)
+
+	def create(self, request, *args, **kwargs):
+		import json
+		# return Response(status=HTTP_202_ACCEPTED)
+		print(json.loads(request.data['formData']))
+		if request.data['Type'] == 'Registration':
+			formData = json.loads(request.data['formData'])
+
+			u = User()
+			u.first_name = formData['name']
+			u.last_name = formData['surname']
+			u.email = formData['email']
+			u.username = formData['username']
+			u.set_password(formData['password'])
+			u.is_active = False
+			u.save()
+			groups = []
+			if formData['role'] == 'user':
+				groups = [Group.objects.get(name='User')]
+			elif formData['role'] == 'owner':
+				groups = [Group.objects.get(name='CinemaOwner')]
+			u.groups.set(groups)
+			u.save()
+
+			return Response(HTTP_200_OK)
+		return Response(HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
