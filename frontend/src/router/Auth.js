53d58e820a8f60a94201a4984576d920f1f1a835
sneakyPent <zaharioudakis@yahoo.com>
2020-10-27 04:23:50 +0200

    add Auth routing js

---
+++ frontend/src/router/Auth.js
@@ -0,0 +1,14 @@
+const basePath = '/auth/';
+
+export default [
+	{
+		path: basePath + 'login',
+		name: 'Login',
+		component: require('../views/Auth/Login').default
+	},
+	{
+		path: basePath + 'register',
+		name: 'Register',
+		component: require('../views/Auth/Register').default
+	}
+];
