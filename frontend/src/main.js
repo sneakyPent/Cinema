e26924f4f2f46ce0f5a5f31483f8fa3898230b64
sneakyPent <zaharioudakis@yahoo.com>
2020-10-27 03:14:56 +0200

    added material design bootstrap package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import * as mdbvue from 'mdbvue';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbvue/lib/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+for (const component in mdbvue) {
+	Vue.component(component, mdbvue[component]);
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -2,4 +2,5 @@ sleep 3
 echo '\033[96mInstalling NPM packages:\033[0m'
 cd frontend
 npm install -g @vue/cli
+npm install git+https://oauth2:TeSGPDu8RY7nX8y-J9N2@git.mdbootstrap.com/mdb/vue/vu-pro.git
 npm run serve -- --port 80
