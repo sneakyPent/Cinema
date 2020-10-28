56fdc9422cc9edc2d6af767c0e5674410b408a91
sneakyPent <zaharioudakis@yahoo.com>
2020-10-28 02:02:50 +0200

    create Home page to be redirected for users when logging in

---
+++ frontend/src/views/Home/welcome.vue
@@ -0,0 +1,20 @@
+<template>
+	<mdb-container>
+		<h1>This is the Homepage!</h1>
+	</mdb-container>
+</template>
+>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+
+<style >
+</style>
