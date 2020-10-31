204936edea7b073ebd6d55b898fdedf94b42234a
sneakyPent <zaharioudakis@yahoo.com>
2020-10-31 05:26:46 +0200

    side nav almost finished (working properly)

--- frontend/src/components/SideNavigationBar.vue
+++ frontend/src/components/SideNavigationBar.vue
@@ -7,91 +7,91 @@
         expand-on-hover
         :slim-collapsed="slimCollapsed"
         @toggleSlim="slimCollapsed = $event"
-        sidenav-class="blue accent-4"
+        sidenav-class="grey"
         color="white"
     >
         <div slot="header">
             <div
                 class="d-flex align-items-center my-4"
-                :class="
-          slimCollapsed
-            ? 'justify-content-center'
-            : 'justify-content-start'
-        "
+                :class="'justify-content-center'"
             >
-                <mdb-avatar :width="40" style="flex: 0 0 auto">
+                <mdb-avatar :width="slimCollapsed ? 40 : 80" style="flex: 0 0 auto">
                     <img
-                        src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg"
-                        class="img-fluid rounded-circle z-depth-1"
+                        src="https://mdbootstrap.com/img/Photos/Avatars/avatar-1.jpg"
+                        class="img-fluid rounded-circle z-depth-3"
                     />
                 </mdb-avatar>
+            </div>
+            <div
+                class="d-flex align-items-center my-4"
+                :class="'justify-content-center'"
+            >
                 <p
                     class="m-t mb-0 ml-4 p-0"
                     style="flex: 0 2 auto"
                     v-show="!slimCollapsed"
                 >
                     <strong
-                    >Ann Smith
-                        <mdb-icon
-                            color="success"
-                            icon="circle"
-                            class="ml-2"
-                            size="sm"
-                        />
+                    ><h6 class="plan-cost white-text">
+                        {{ this.userData.last_name + ' ' + this.userData.first_name }}</h6>
+                    </strong>
+                </p>
+            </div>
+            <div
+                class="d-flex align-items-center my-4"
+                :class="'justify-content-center'"
+            >
+                <p
+                    class="m-t mb-0 ml-4 p-0"
+                    style="flex: 0 2 auto"
+                    v-show="!slimCollapsed"
+                >
+                    <strong
+                    ><h6 class="plan-cost white-text">
+                        ({{ this.userData.role }})</h6>
                     </strong>
                 </p>
             </div>
             <hr class="w-100"/>
-            <mdb-nav-item @click="logout">
-                <mdb-icon icon="sign-out-alt" class="mr-1"/>
-            </mdb-nav-item>
         </div>
     </mdb-side-nav-2>
 </template>
 <script>
-import {mdbSideNav2, mdbIcon, mdbAvatar} from "mdbvue";
 
 export default {
     name: "SideNavigationBar",
-    components: {
-        mdbIcon,
-        mdbAvatar,
-        mdbSideNav2
-    },
-    methods: {
-        logout: function () {
-            this.$session.destroy();
-            window.location.reload(true);
+    computed: {
+        navigation: function () {
+            let nv = [];
+            let rt = this.$root.routes.filter(route => {
+                return 'sidenav' in route;
+            });
+            for (let i = 0; i < rt.length; i++) {
+                nv.push(rt[i].sidenav)
+            }
+            return nv;
         },
     },
     data() {
         return {
             show: true,
             slimCollapsed: true,
-            navigation: [
-                {
-                    name: "Home",
-                    to: "/",
-                    icon: "home"
-                },
-                {
-                    name: "CSS",
-                    to: "/css",
-                    icon: "css3",
-                    fab: true
-                },
-                {
-                    name: "Components",
-                    to: "/components",
-                    icon: "cubes"
-                },
-                {
-                    name: "Advanced",
-                    to: "/advanced",
-                    icon: "code"
-                }
-            ]
+            userData: {}
         };
+    },
+    methods: {
+        initData: function() {
+            const accessToken = this.$session.get('access');
+            const tokenData = this.$root.parseJwt(accessToken);
+            this.$axios.get('http://localhost:8000/api/UserProfile/' + tokenData.user_id + '/')
+                .then(res => {
+                    this.userData = res.data;
+                })
+                .catch(this.$notifyAction.error);
+        },
+    },
+    mounted: function() {
+        this.initData()
     }
 };
 </script>
@@ -100,5 +100,9 @@ export default {
 .navbar i {
     cursor: pointer;
     color: white;
+}
+
+.background-color {
+    color: #9F9F9F;
 }
 </style>
