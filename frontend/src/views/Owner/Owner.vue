94c8e0bb512d6c93f6da659eca30f2eaee6a2e4f
sneakyPent <zaharioudakis@yahoo.com>
2020-10-31 05:24:40 +0200

    added Movies and owner vue pages (still blank)

---
+++ frontend/src/views/Owner/Owner.vue
@@ -0,0 +1,15 @@
+<template>
+	<mdb-container>
+		<h1>This is the owner page!</h1>
+	</mdb-container>
+</template>
+
+<script>
+export default {
+name: "Owner"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/User/Movies.vue
@@ -0,0 +1,15 @@
+<template>
+	<mdb-container>
+		<h1>This is the movies page!</h1>
+	</mdb-container>
+</template>
+
+<script>
+export default {
+name: "Movies"
+}
+</script>
+
+<style scoped>
+
+</style>
