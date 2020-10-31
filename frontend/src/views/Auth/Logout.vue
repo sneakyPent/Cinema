6e5cc0583bdd2ff99b71fde8ca98b136f61e2558
sneakyPent <zaharioudakis@yahoo.com>
2020-10-31 05:24:05 +0200

    create logout vue for destroying session and redirects to login page

---
+++ frontend/src/views/Auth/Logout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+
+    name: 'Logout',
+    mounted: function () {
+        this.$session.destroy();
+        window.location.reload(true);
+    },
+}
+</script>
