626c67811c09a9a67bb1bd6bce658981569ffc77
sneakyPent <zaharioudakis@yahoo.com>
2020-11-01 03:48:57 +0200

    change database DATE_INPUT_FORMATS to allow = ['%d/%m/%Y']

--- cinema/settings.py
+++ cinema/settings.py
@@ -136,9 +136,12 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_INPUT_FORMATS = ['%d/%m/%Y']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
