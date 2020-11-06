ed915dae4055b1b20735502ec24ef38a8146e4b5
sneakyPent <zaharioudakis@yahoo.com>
2020-11-06 16:31:30 +0200

    minor changes to frontend.sh

--- scripts/frontend.sh
+++ scripts/frontend.sh
@@ -1,6 +1,8 @@
-sleep 3
+sleep 6
 echo '\033[96mInstalling NPM packages:\033[0m'
 cd frontend
-npm install -g @vue/cli
-npm install git+https://oauth2:TeSGPDu8RY7nX8y-J9N2@git.mdbootstrap.com/mdb/vue/vu-pro.git
+#npm install
+#npm install --save jquery
+#npm install -g @vue/cli
+#npm install git+https://oauth2:TeSGPDu8RY7nX8y-J9N2@git.mdbootstrap.com/mdb/vue/vu-pro.git
 npm run serve -- --port 80
