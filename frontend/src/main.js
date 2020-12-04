93e760074006283a3350cdada259a93e5d05f960
sneakyPent <zaharioudakis@yahoo.com>
2020-12-04 23:53:05 +0200

    delete parseJwt in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -86,20 +86,6 @@ const vm = new Vue({
                 this.$axios.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
             }
         },
-        parseJwt: function (token) {
-            const base64Url = token.split('.')[1];
-            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-            const jsonPayload = decodeURIComponent(
-                atob(base64)
-                    .split('')
-                    .map(function (c) {
-                        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-                    })
-                    .join('')
-            );
-
-            return JSON.parse(jsonPayload);
-        },
     },
 });
 
