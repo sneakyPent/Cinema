f37f22b3c24354c996b2fcfabcf98a28d5ce72da
sneakyPent <zaharioudakis@yahoo.com>
2020-12-06 23:30:36 +0200

    update Logout.vue to revoke access token

--- frontend/src/views/Auth/Logout.vue
+++ frontend/src/views/Auth/Logout.vue
@@ -11,6 +11,21 @@ export default {
 
     name: 'Logout',
     mounted: function () {
+        let qs = require('qs');
+        let headers= {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + btoa(this.$getClient_id()+ ':' + this.$getClient_secret())
+            }
+        let tok = {
+            'token': this.$session.get('access'),
+            'token_type_hint': 'access_token',
+        }
+        this.$axios.post('/oauth2/revoke', qs.stringify(tok) , {headers: headers})
+            .then(res => {
+                console.log(res);
+
+            })
+            .catch()
         this.$session.destroy();
         window.location.reload(true);
     },
